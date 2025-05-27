79dd37913c21e0cd924241cf054ffa65b62572a4
Morainy-Henri <morainyhenri@gmail.com>
2025-05-27 11:08:28 +0300

    Le refresh token est maintenant fonctionnel

--- backend/authentification/urls.py
+++ backend/authentification/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from .views import LoginView, current_user
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('me/', current_user, name='current_user'),  
+    path('me/', current_user, name='current_user'),
+
+    # Endpoints JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),       # Pour obtenir access + refresh
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),      # Pour rafraîchir le token access
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),         # Pour vérifier un token JWT
 ]
